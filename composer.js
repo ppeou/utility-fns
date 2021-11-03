3187d52623bcbee651dc6f1e5261cb8a97b29d84
Bong GitHub <ppeou@msn.com>
2021-11-02 19:34:34 -0500

    Update composer.js

--- composer.js
+++ composer.js
@@ -2,7 +2,7 @@ const doA = (input) => `${input} -> doA`;
 const doB = (input) => `${input} -> doB`;
 const doC = (input) => `${input} -> doC`;
 
-const compose = (fns) => {
+const composerStyle1 = (fns) => {
   return (input) => {
     return fns.reduce((p, fn) => {
       return fn(p);
@@ -10,9 +10,18 @@ const compose = (fns) => {
   };
 };
 
+const composerStyle2 = (...fns) => {
+  return (input) => {
+    return fns.reduce((p, fn) => {
+      return fn(p);
+    }, input);
+  };
+};
 
-const composedFns = compose([doA,doB,doC]);
+const composedFns1 = composerStyle1([doA,doB,doC]);
+const output1 = composedFns1('composerStyle1');
+console.log(output1); //composerStyle1 -> doA -> doB -> doC
 
-const output = composedFns('hello');
-
-console.log(output); //hello -> doA -> doB -> doC
+const composedFns2 = composerStyle1(doA,doB,doC);
+const output2 = composedFns1('composerStyle2');
+console.log(output2); //composerStyle2 -> doA -> doB -> doC
